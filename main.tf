f050a5558da8c1c09a1c652f4fa3197dc5162f6f
Nathan Kuik <nkuik@toogoodtogo.com>
2021-04-11 15:08:11 +0200

    Push example temp

--- main.tf
+++ main.tf
@@ -116,3 +116,52 @@ resource "aws_lambda_permission" "this" {
 output "api_gateway_endpoint" {
   value = aws_apigatewayv2_api.this.api_endpoint
 }
+
+resource "aws_route53_zone" "zone" {
+  name = "terraform-aws-api-gateway-lambda-demo.com"
+}
+
+resource "aws_acm_certificate" "zone" {
+  domain_name = "*.${aws_route53_zone.zone.name}"
+  validation_method = "DNS"
+}
+
+resource "aws_apigatewayv2_domain_name" "this" {
+  domain_name = "terraform-aws-api-gateway-lambda-demo.com"
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate.zone.arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "this" {
+  name    = aws_apigatewayv2_domain_name.this.domain_name
+  type    = "A"
+  zone_id = aws_route53_zone.zone.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  domain_name = aws_apigatewayv2_domain_name.this.id
+  stage       = aws_apigatewayv2_stage.this.id
+}
+
+
+# resource "aws_route53_record" "wildcard" {
+#   zone_id = aws_route53_zone.zone.zone_id
+#   name    = "*.${var.aws_account_name}.come"
+#   type    = "CNAME"
+#   ttl     = "300"
+
+#   records = [
+#     "internal.${var.aws_account_name}.tgtg.ninja",
+#   ]
+# }
